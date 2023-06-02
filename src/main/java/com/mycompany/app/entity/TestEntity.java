bb65037ef472e0d287ec808dd2260b944100210c
Your Name <you@example.com>
2023-06-01 20:35:58 -0500

    Nombre del Proyecto: my-app, Descripcion: version final del proyecto my-app

---
+++ src/main/java/com/mycompany/app/entity/TestEntity.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.mycompany.app.entity;
+
+/**
+ * @author Juan Eduardo
+ *
+ */
+public class TestEntity {
+
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
